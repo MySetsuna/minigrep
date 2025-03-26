6acc3a87e3c9dcb25e4244f616ddd3339ba26012
Jack Jiang <jxk2setsuna@gmail.com>
2025-03-27 01:41:36 +0800

    tijao

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
     // let config = parse_config(&args);
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem pasring argument {err}");
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
     let contents =
         fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("With text:/{contents}");
+    println!("With text:\n/{contents}");
 }
 
 struct Config {
@@ -25,7 +28,7 @@ fn parse_config(args: &[String]) -> Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
